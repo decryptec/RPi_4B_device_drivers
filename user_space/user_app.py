30a85a3f8c81d5fc586d4a63f376ee5b63d6a668
decryptec <dhsdon47@outlook.com>
2025-03-15 23:07:10 -0700

    read python

---
+++ user_space/user_app.py
@@ -0,0 +1,9 @@
+def main():
+    driver_handle = open('/proc/my_driver')
+    message_from)kernel_space = driver_handle.readline()
+    print(message_from_kernel_space)
+    driver_handle.close()
+    return 0
+
+if __name__ == "__main__":
+    main()
