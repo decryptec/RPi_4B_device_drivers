f0f0f3b896b580a633a730496d41a92bcfeddc17
Don Huynh <125177998+decryptec@users.noreply.github.com>
2025-03-18 21:45:58 -0700

    Update test.c

--- 06_open_release/test.c
+++ 06_open_release/test.c
@@ -3,30 +3,29 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(int argc, char **argv){
-	int fd;
+int main(int argc, char **argv) {
+    int fd;
 
-	if (argc < 2){
-		printf("File requires to open as an argument\n");
-		return 0;
-	}
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-	fd = open(argv[1], O_RDONLY);
+    // Open the file in read-only mode
+    fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        perror("Error opening file for reading");
+        return EXIT_FAILURE;
+    }
+    close(fd);
 
-	if(fd < 0){
-		perror("open");
-		return fd;
-	}
+    // Open the file in write-only mode, non-blocking
+    fd = open(argv[1], O_WRONLY | O_NONBLOCK);
+    if (fd < 0) {
+        perror("Error opening file for writing");
+        return EXIT_FAILURE;
+    }
 
-	close (fd);
-
-	fd = open(argv[1], O_WRONLY | O_NONBLOCK);
-
-	if(fd < 0){
-		perror("open");
-		return fd;
-	}
-
-	close(fd);
-	return 0;
+    close(fd);
+    return 0;
 }
