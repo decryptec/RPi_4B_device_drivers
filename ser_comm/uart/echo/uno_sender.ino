050c2feab2f7f647142b19a77b61dc4af81c9547
Don Huynh <125177998+decryptec@users.noreply.github.com>
2025-04-23 00:25:43 -0700

    Update uno_sender.ino

--- ser_comm/uart/echo/uno_sender.ino
+++ ser_comm/uart/echo/uno_sender.ino
@@ -1,23 +1,43 @@
-const int buttonPin = 2;  // the number of the pushbutton pin
+const int buttonPin = 2;     // Pushbutton pin
+const int ledPin = 13;       // Built-in LED pin
 
-int buttonState = 0;  // variable for reading the pushbutton status
+bool ledState = false;
+bool lastButtonState = LOW;
 
 void setup() {
   pinMode(buttonPin, INPUT);
-  Serial.begin(9600);
-  // Wait for serial connection to be established
-  while (!Serial) {
-    ; // Wait for serial to be ready
-  }
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial);  // Wait for Serial to be ready
 }
 
 void loop() {
-    buttonState = digitalRead(buttonPin);
-    if (buttonState == HIGH) {
-      Serial.println("BUTTON_PRESSED");
-    } else {
-      Serial.println("BUTTON_RELEASED");
-    }
+  // Check for message from Raspberry Pi
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    input.trim();
 
-  delay(100); // Debounce and rate limit
+    if (input == "ON") {
+      ledState = true;
+      digitalWrite(ledPin, HIGH);
+    } else if (input == "OFF") {
+      ledState = false;
+      digitalWrite(ledPin, LOW);
+    }
+  }
+
+  // Only send button state if LED is on
+  if (ledState) {
+    bool currentButtonState = digitalRead(buttonPin);
+    if (currentButtonState != lastButtonState) {
+      lastButtonState = currentButtonState;
+      if (currentButtonState == HIGH) {
+        Serial.println("PRESSED");
+      } else {
+        Serial.println("RELEASED");
+      }
+    }
+  }
+
+  delay(50); // Debounce & pacing
 }
